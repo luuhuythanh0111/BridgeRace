34c7be3123b66c3767038bc439caee13b1a2df5f
luuhuythanh <luuhuythanh0111@gmail.com>
2023-04-11 18:59:41 +0700

    Revert "Merge conflicts resolved"
    This reverts commit 11fa9b0c5ca85452f5838c9cb6bb686c8b6ff8a1.

--- Assets/_Game/Scripts/Bot.cs
+++ Assets/_Game/Scripts/Bot.cs
@@ -11,6 +11,7 @@ public class Bot : Character
 
     private IState currentState;
 
+<<<<<<< Updated upstream
     private int currentPlatformIndex;
 
     internal Vector3 targetBrickPosition = Vector3.zero;
@@ -28,8 +29,11 @@ public class Bot : Character
         currentPlatformIndex = 0;
         //Debug.Log(randomTargetBrick);
     }
+=======
+    private Vector3 targetPosition = Vector3.zero;
 
     private bool isMoving = false;
+>>>>>>> Stashed changes
 
     protected override void Update()
     {
@@ -68,6 +72,7 @@ public class Bot : Character
 
     public void Moving()
     {
+<<<<<<< Updated upstream
         if (randomTargetBrick <= playerBody.GetComponent<PlayerControlBricks>().bricks.Count)
         {
             ChangeState(new BuildBridgeState());
@@ -140,16 +145,17 @@ public class Bot : Character
             ChangeState(new PatrolState());
             haveGetDown = false;
         }
-
+=======
         if (isMoving == false)
         {
             horizontalInput = Random.Range(-1f, 1f);
-            verticalInput = (Random.Range(0, 2) == 0 ? 1 : -1);
+            verticalInput = (Random.Range(0, 2) == 0 ? 1 : -1)
             
             isMoving = true;
         }
         
         MovePlayer();
 
+>>>>>>> Stashed changes
     }
 }
