1035c1958f7ab05fd1700a0690594a9e17011b8f
luuhuythanh <luuhuythanh0111@gmail.com>
2023-04-11 21:45:24 +0700

    update 11/4

--- Assets/_Game/Scripts/SpawnBricks.cs
+++ Assets/_Game/Scripts/SpawnBricks.cs
@@ -1,78 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnBricks : MonoBehaviour
 {
     [SerializeField] private GameObject prefabBrick;
-
+    private bool haveSpawn = false;
     private bool[] haveSpawnBrickColor = new bool[10];
-    private bool[,] haveSpawnThisCoordinate = new bool[21, 21];
+    internal bool[,] haveSpawnThisCoordinate = new bool[21, 21];
     private float[,] randomSpawnTime = new float[21, 21];
     private float[,] timer = new float[21, 21];
+    private List<int> usedColorIndex = new List<int>();
+    public int randomRange;
+    private void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            haveSpawnBrickColor[i] = false;
+        }
 
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                haveSpawnThisCoordinate[i, j] = false;
+                randomSpawnTime[i, j] = Random.Range(5f, 8f);
+            }
+        }
+    }
     public void SpawnBrick(int colorIndex)
     {
-        if (haveSpawnBrickColor[colorIndex] ==false)
+        if (haveSpawnBrickColor[colorIndex] == false)
         {
-            for(int i=1; i<=20; i++)
-            {
-                for (int j = 1; j <= 20; j++)
-                {
-                    if (haveSpawnThisCoordinate[i, j] == true)
-                        continue;
-                    int x = Random.Range(0, FindObjectOfType<LevelManager>().indexUsingColor.Count);
+            usedColorIndex.Add(colorIndex);
 
-                    if (FindObjectOfType<LevelManager>().indexUsingColor[x] != colorIndex) 
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    if (haveSpawnThisCoordinate[i, j])
+                    {
                         continue;
+                    }
+                    
+                    int x = Random.Range(0, FindObjectOfType<LevelManager>().usedCololIndex.Count + 1);
+                    if (x == FindObjectOfType<LevelManager>().usedCololIndex.Count)
+                    {
+                        continue;
+                    }
+                    x = FindObjectOfType<LevelManager>().usedCololIndex[x];
+                    
+                    if (x != colorIndex)
+                    {
+                        continue;
+                    }
+                    //Debug.Log(colorIndex + " " + x);
                     GameObject brick = Instantiate(prefabBrick, Vector3.zero, Quaternion.identity, transform.GetChild(0));
                     brick.transform.GetChild(0).GetComponent<Renderer>().sharedMaterial = FindObjectOfType<LevelManager>().GetMaterialFromNumber(colorIndex);
-                    brick.transform.localPosition = new Vector3(25 - 2*i, 0, 25 - 2*j);
+                    brick.transform.localPosition = new Vector3(10 - 2 * i, 0, 10 - 2 * j);
+                    brick.GetComponent<Brick>().x = i;
+                    brick.GetComponent<Brick>().y = j;
                     haveSpawnThisCoordinate[i, j] = true;
-
-                    randomSpawnTime[i,j] = Random.Range(2f, 5f);
                 }
             }
             haveSpawnBrickColor[colorIndex] = true;
         }
-    }
-
-    void CreateRandomTime()
-    {
-        for (int i = 1; i <= 20; i++)
+        else
         {
-            for (int j = 1; j <= 20; j++)
-                randomSpawnTime[i,j] = Random.Range(2f, 5f);
+            SpawnOnUpdate();
         }
     }
-
     void SpawnOnUpdate()
     {
-        for (int i = 1; i <= 20; i++)
+        for (int i = 1; i <= 10; i++)
         {
-            for (int j = 1; j <= 20; j++)
+            for (int j = 1; j <= 10; j++)
             {
-                timer[i, j] += Time.deltaTime;
-
+                if (haveSpawnThisCoordinate[i, j])
+                    continue;
+                timer[i, j] += Time.deltaTime/(usedColorIndex.Count*1f);
                 if (timer[i, j] > randomSpawnTime[i, j])
                 {
                     timer[i, j] = 0;
-                    randomSpawnTime[i, j] = Random.Range(5f, 10f);
-                    int x = FindObjectOfType<LevelManager>().indexUsingColor[
-                        Random.Range(0, FindObjectOfType<LevelManager>().indexUsingColor.Count)];
-
-                    Debug.Log(x);
-
+                    randomSpawnTime[i, j] = Random.Range(5f, 8f);
+                    int x = Random.Range(0, usedColorIndex.Count);
+                    x = usedColorIndex[x];
                     GameObject brick = Instantiate(prefabBrick, Vector3.zero, Quaternion.identity, transform.GetChild(0));
                     brick.transform.GetChild(0).GetComponent<Renderer>().sharedMaterial = FindObjectOfType<LevelManager>().GetMaterialFromNumber(x);
-                    brick.transform.localPosition = new Vector3(25 - 2 * i, 0, 25 - 2 * j);
+                    brick.transform.localPosition = new Vector3(10 - 2 * i, 0, 10 - 2 * j);
+                    brick.GetComponent<Brick>().x = i;
+                    brick.GetComponent<Brick>().y = j;
+                    haveSpawnThisCoordinate[i, j] = true;
                 }
             }
         }
-    }
-
-    private void Update()
-    {
-        SpawnOnUpdate();
+        
     }
 }
